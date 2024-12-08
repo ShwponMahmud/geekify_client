64454302863ceff0d734447f92aa2f8bf26d220e
krazytobi <tobi@krazyit.com>
2024-12-08 17:56:49 +0600

    franchise page updated

---
+++ src/app/(root)/franchise/page.tsx
@@ -0,0 +1,12 @@
+import FranchiseComponents from '@/app/components/FranchiseComponents/FranchiseComponents'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <FranchiseComponents/>
+    </div>
+  )
+}
+
+export default page
