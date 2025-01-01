69ec8825ae6888a9f9112b6327106d4310322ef6
Shwpon Mahmud <shwpon@krazyit.com>
2025-01-01 10:10:39 +0600

    payment success create

---
+++ src/app/components/BookingOnlineComponents/PaymentSuccess/PaymentSuccess.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PaymentSuccess() {
+  let appointmentMessage = 'Your booking has been confirmed. Please check your email/phone for details!';
+  let appointmentUndecidedMessage = 'Your request successfully submitted. Our team members will contact you soon!';
+  return (
+    <div>
+      
+    </div>
+  )
+}
